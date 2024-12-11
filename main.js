37ee3bc0350e457c1ba831c1a84fc08ed880c0e7
MelanyRodriguez <melanyrc.19@gmail.com>
2024-12-11 10:25:44 -0300

    no me funciona cambio de colores

--- main.js
+++ main.js
@@ -5,26 +5,23 @@ function $(element) {
 const $inputTextoSuperior = $("#textoSuperior")
 const $inputTextoInferior = $("#textoInferior")
 
-const $sinTexto1 = $ ("#sinTextoSuperior")
-const $sinTexto2 = $ ("#sinTextoInferior")
-const $inputFuente = $ ("#fuente")
-const $inpurUrl= $("#inputUrl")
-const $inputFamilia = $ ("#familiaFuente")
-const $inputTamañoFuente = $ ("#tamañoFuente")
-const $inputAlinearTexto = $ ("#alinearTexto")
-// const $inpurOptionIzquierda = $ ("#textoIzquieda")
-// const $inpurOptionCentro = $ ("#textoCentro")
-// const $inpurOptionDerecha = $ ("#textoDerecha")
-const $inputColorTexto = $ ("#colorTexto")
-const $inputColorFondo = $ ("#colorFondo")
+const $sinTexto1 = $("#sinTextoSuperior")
+const $sinTexto2 = $("#sinTextoInferior")
+const $inputFuente = $("#fuente")
+const $inputUrl= $("#inputUrl")
+const $inputFamilia = $("#familiaFuente")
+const $inputTamañoFuente = $("#tamañoFuente")
+const $inputAlinearTexto = $("#alinearTexto")
+const $inputColorTexto = $("#colorTexto")
+const $inputColorFondo = $("#colorFondo")
 const $inputTransparente = $("#fondoTransparente")
-const $inputPading = $ ("#padingTexto")
-const $inputTamañoLinea = $ ("#tamañoLinea")
+const $inputPading = $("#padingTexto")
+const $inputTamañoLinea = $("#tamañoLinea")
 
 const $h1Texto = $("#primerTexto")
 const $h1SegundoTexto = $("#segundoTexto")
 const $imagen = $("#imagenMeme");
-const $cardTexto = ("#card")
+const $cardTexto = $("#card")
 
 $inputTextoSuperior.addEventListener ("input", (e)  => {
 $h1Texto.innerText = e.target.value
@@ -34,41 +31,39 @@ $inputTextoInferior.addEventListener ("input", (e) => {
 $h1SegundoTexto.innerText = e.target.value
 })
 
-if ($sinTexto1) {  
-    $sinTexto1.addEventListener("click", (e) => {
-        $h1Texto.style.display = "none";  
+
+$sinTexto1.addEventListener("click", (e) => {
+$h1Texto.style.display = "none";  
     });
-}
 
 
-if ($sinTexto2){
-    $sinTexto2.addEventListener ("click", (e) => {
-        $h1SegundoTexto.style.display = "none";
+
+
+ $sinTexto2.addEventListener ("click", () => {
+ $h1SegundoTexto.style.display = "none";
     })
-}
+
 
 
 $inputFuente.addEventListener ("input", (e) => {
-$sinTexto1.innerText =  e.target.value;
-$sinTexto2.innerText =  e.target.value
+$h1Texto.innerText =  e.target.value;
+$h1SegundoTexto.style.fontFamily =  e.target.value
 })
 
-if ($inpurUrl){
-$inpurUrl.addEventListener ("input", (e) => {
-    $imagen.src = e.target.value});
-    $imagen.style.maxWidth = "100%"; 
-    $imagen.style.maxHeight = "100%"; 
-    $imagen.style.objectFit = "contain"; 
-}
+// if ($inputUrl){
+// $inputUrl.addEventListener ("input", (e) => {
+//     $imagen.src = e.target.value});
+//     $imagen.style.maxWidth = "100%"; 
+//     $imagen.style.maxHeight = "100%"; 
+//     $imagen.style.objectFit = "contain"; 
+// }
 
 $inputTamañoFuente.addEventListener ("input", () => {
-    $h1Texto.style.fontZise = $inputTamañoFuente.value;
-    $h1SegundoTexto.style.fontZise = $inputTamañoFuente.value;
+    $h1Texto.style.fontSize = $inputTamañoFuente.value + "px";
+    $h1SegundoTexto.style.fontSize = $inputTamañoFuente.value + "px";
 
 })
-
-
-$inputColorTexto.addEventListener("input", () => {
+$inputColorTexto.addEventListener("input", (e) => {
     $h1Texto.style.color = $inputColorTexto.value;
     $h1SegundoTexto.style.color = $inputColorTexto.value
 })
